7889fd8720832d9d780dd1d999ec14910ada6bd0
Sunghoon Jung <younsol.r@nexon-gt.com>
2016-10-24 16:30:22 +0900

    SqlServerDatabase는 runtime에 객체를 생성해야 connection이 생긴다 ㅠㅠ

--- ChatProtoDatabase/ChatProtoSqlServer.cs
+++ ChatProtoDatabase/ChatProtoSqlServer.cs
@@ -4,7 +4,20 @@ namespace ChatProtoDatabase
 {
     public sealed class ChatProtoSqlServer : SqlServerDatabase
     {
-        public static ChatProtoSqlServer Instance = new ChatProtoSqlServer();
+        private static ChatProtoSqlServer instance;
+        private static object instanceLock = new object();
+        public static ChatProtoSqlServer Instance
+        {
+            get {
+                lock(instanceLock)
+                {
+                    if (instance == null)
+                        instance = new ChatProtoSqlServer();
+                }
+                return instance;
+            }
+        }
+
         private static readonly string DatabaseConnectionString
             = "Data Source=GT15767-I1;Initial Catalog=ChatProtoDb;Persist Security Info=True;User ID=sa;Password=1q2w#E$R";
 
